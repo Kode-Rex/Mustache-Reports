572ff0c4d14fea5bbe6e1643e258e8a89818bd49
T-rav <tmfrisinger@gmail.com>
2017-07-17 16:05:28 +0200

    Updated Sync.Process.Runner version

--- source/Simple.Report.Server.Example/Program.cs
+++ source/Simple.Report.Server.Example/Program.cs
@@ -13,6 +13,23 @@ using TddBuddy.Synchronous.Process.Runner;
 
 namespace Simple.Report.Server.Example
 {
+    /*
+     * This is a working example of how to generate PDF reports using node and LibreOffice
+     * The word file happens using docx-templater written in node to genereate docx reports based on a template
+     * The pdf conversion happens using LibreOffice, both node and libreoffice 5 need to be installed on your computer to run this example.
+     * 
+     * Please note there is a pre-build hook to copy the NodeApp contents into the solution directory Simple.Report.Server.Example\Reporting\NodeApp from Simple.Report.Server.NodeJS
+     * 
+     * Please also note this example makes use of TddBuddy's Clean Architecture and Syncronus Process Execution packages. 
+     * 
+     * + Synchronous Process Runner - DotNetCore
+     *  - Install-Package TddBuddy.Synchronous.Process.Runner.DotNetCore
+     *  - Source @ https://github.com/T-rav/Synchronous-Process-Runner-DotNetCore
+     *  
+     * + Clean Architecture Domain - DotNetCore
+     *  - Install-Package TddBuddy.CleanArchitecture.Domain.DotNetCore
+     *  - Source @ https://github.com/T-rav/CleanArchitecture-DotNetCore.git
+     */
     class Program
     {
         private static readonly IRenderReportUseCase RenderReportUseCase = new RenderReportUseCase(new ReportRepository("Reporting\\Templates","Reporting\\NodeApp"));
@@ -20,7 +37,7 @@ namespace Simple.Report.Server.Example
         static void Main(string[] args)
         {
             // todo : change this when running example for different output location
-            var writeReportTo = "c:\\SimpleReportServer.RenderedReports";
+            var writeReportTo = "C:\\SimpleReportServer.RenderedReports";
             RenderReportWithImages(writeReportTo);
         }
 
@@ -55,6 +72,9 @@ namespace Simple.Report.Server.Example
             }
 
             Console.WriteLine($"Report output to [{pdfPresenter.SuccessContent}]");
+            Console.WriteLine("");
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
         }
 
         private static PropertyPresenter<string, ErrorOutputMessage> RenderReportToPdf(string reportOuputDirectory, string reportPath)
