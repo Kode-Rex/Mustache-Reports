f8235bda194ad9d9d550015e75cf476a892e4b3c
T-rav <tmfrisinger@gmail.com>
2018-02-15 10:56:59 +0200

    Reorder code to better sperate out packages from example

---
+++ stage-packages.bat
@@ -0,0 +1,3 @@
+copy source\Mustache.Reports.Boundry\bin\Debug\*.nupkg nuget-packages
+copy source\Mustache.Reports.Data\bin\Debug\*.nupkg nuget-packages
+copy source\Mustache.Reports.Domain\bin\Debug\*.nupkg nuget-packages
