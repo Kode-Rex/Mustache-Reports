12002ae3d82b531f40270a857551ca72239307c7
T-rav <tmfrisinger@gmail.com>
2018-02-16 22:09:22 +0200

    Fixed Data and Domain nuget packages based on boundry package changes

--- source/Mustache.Reports.Data.Tests/ReportGatewayTests.cs
+++ source/Mustache.Reports.Data.Tests/ReportGatewayTests.cs
@@ -8,7 +8,7 @@ namespace Mustache.Reports.Data.Tests
     public class ReportGatewayTests
     {
         [Fact]
-        public void CreateReport_WhenValidInput_ShouldReturnRenderedReport()
+        public void CreateWordReport_WhenValidInput_ShouldReturnRenderedReport()
         {
             //---------------Arrange------------------
             var configuration = SetupConfiguration();
@@ -23,12 +23,41 @@ namespace Mustache.Reports.Data.Tests
         }
 
         [Fact]
-        public void CreateReport_WhenInvalidTemplateName_ShouldReturnTemplateNameError()
+        public void CreateWordReport_WhenInvalidTemplateName_ShouldReturnTemplateNameError()
         {
             //---------------Arrange------------------
             var configuration = SetupConfiguration();
             var wordGateway = new ReportGateway(configuration);
             var input = new RenderWordInput { JsonModel = "", ReportName = "test.docx", TemplateName = "INVALID_NAME" };
+            //---------------Act----------------------
+            var actual = wordGateway.CreateWordReport(input);
+            //---------------Assert-------------------
+            Assert.True(actual.HasErrors());
+            Assert.Equal("Invalid Report Type [INVALID_NAME]", actual.ErrorMessages[0]);
+        }
+
+        [Fact]
+        public void CreateExcelReport_WhenValidInput_ShouldReturnRenderedReport()
+        {
+            //---------------Arrange------------------
+            var configuration = SetupConfiguration();
+            var reportData = File.ReadAllText(configuration["Reporting:RelativeSampleDataLocation"]);
+            var wordGateway = new ReportGateway(configuration);
+            var input = new RenderWordInput { JsonModel = reportData, ReportName = "test.xslx", TemplateName = "SimpleReport" };
+            //---------------Act----------------------
+            var actual = wordGateway.CreateWordReport(input);
+            //---------------Assert-------------------
+            var expected = File.ReadAllText("Expected\\RenderedWordBase64.txt");
+            Assert.Equal(expected.Substring(0, 50), actual.Base64String.Substring(0, 50));
+        }
+
+        [Fact]
+        public void CreateExcelReport_WhenInvalidTemplateName_ShouldReturnTemplateNameError()
+        {
+            //---------------Arrange------------------
+            var configuration = SetupConfiguration();
+            var wordGateway = new ReportGateway(configuration);
+            var input = new RenderWordInput { JsonModel = "", ReportName = "test.xslx", TemplateName = "INVALID_NAME" };
             //---------------Act----------------------
             var actual = wordGateway.CreateWordReport(input);
             //---------------Assert-------------------
