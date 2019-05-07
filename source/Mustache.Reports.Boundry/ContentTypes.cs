35cfacf28f3e24c69f47d3beb05bc8e7950b8e9d
T-rav <t@stoneagetechnologies.com>
2019-05-07 09:41:17 -0700

    feat: added Csv content type

--- source/Mustache.Reports.Boundry/ContentTypes.cs
+++ source/Mustache.Reports.Boundry/ContentTypes.cs
@@ -9,5 +9,11 @@ namespace Mustache.Reports.Boundary
         public static ContentType Excel = new ContentType {Value = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"};
         public static ContentType Word = new ContentType{Value = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"};
         public static ContentType Pdf = new ContentType{Value = "application/pdf"};
+        public static ContentType Csv = new ContentType { Value = "text/csv" };
+
+        public static ContentType Make_Custom_Type(string type)
+        {
+            return new ContentType {Value = type};
+        }
     }
 }
